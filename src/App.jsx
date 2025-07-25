23baae7d8a11f0a7cf15fd6c0fb083915fa26c35
Sonu Kumar Sah <sonusah22388@gmail.com>
2025-07-26 02:46:04 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBooking from './pages/MyBooking'
+import Favorite from './pages/Favorite'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
+
+const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+        <Route path='/my-bookings' element={<MyBooking/>} />
+        <Route path='/favorite' element={<Favorite/>} />
+      </Routes>
+      {!isAdminRoute && <Footer/>}
+    </>
   )
 }
 
