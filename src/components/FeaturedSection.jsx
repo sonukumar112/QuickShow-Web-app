b8821c2b5ca1f36a75ab1aaa0b053593f20187aa
Sonu Kumar Sah <sonusah22388@gmail.com>
2025-07-27 00:05:32 +0530

    added code

---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,21 @@
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+const FeaturedSection = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
+
+      <div>
+        <p>Now Showing</p>
+        <button>View All <ArrowRight /></button>
+      </div>
+
+      <div></div>
+
+      <div></div>
+      
+    </div>
+  )
+}
+
+export default FeaturedSection
